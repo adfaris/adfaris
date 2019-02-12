f1c9afc2a5e0109385f4c85713a53030dadf0e3a
A.D.Faris <adfaris@gmail.com>
2019-02-11 23:50:44 -0500

    layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,52 +25,24 @@ const DEFAULT_IMAGES = [
     src: full01,
     thumbnail: thumb01,
     caption: 'Photo 1',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+    description: 'Music review.'
   },
   {
     id: '2',
     src: full02,
     thumbnail: thumb02,
     caption: 'Photo 2',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-  },
-  {
-    id: '3',
-    src: full03,
-    thumbnail: thumb03,
-    caption: 'Photo 3',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-  },
-  {
-    id: '4',
-    src: full04,
-    thumbnail: thumb04,
-    caption: 'Photo 4',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-  },
-  {
-    id: '5',
-    src: full05,
-    thumbnail: thumb05,
-    caption: 'Photo 5',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-  },
-  {
-    id: '6',
-    src: full06,
-    thumbnail: thumb06,
-    caption: 'Photo 6',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-  },
+    description: 'Lambda notes.'
+  }
 ]
 
 class HomeIndex extends React.Component {
-  constructor() {
+  constructor () {
     super()
 
     this.state = {
       lightboxIsOpen: false,
-      currentImage: 0,
+      currentImage: 0
     }
 
     this.closeLightbox = this.closeLightbox.bind(this)
@@ -80,36 +52,36 @@ class HomeIndex extends React.Component {
     this.handleClickImage = this.handleClickImage.bind(this)
   }
 
-  openLightbox(index, event) {
+  openLightbox (index, event) {
     event.preventDefault()
     this.setState({
       currentImage: index,
-      lightboxIsOpen: true,
+      lightboxIsOpen: true
     })
   }
-  closeLightbox() {
+  closeLightbox () {
     this.setState({
       currentImage: 0,
-      lightboxIsOpen: false,
+      lightboxIsOpen: false
     })
   }
-  gotoPrevious() {
+  gotoPrevious () {
     this.setState({
-      currentImage: this.state.currentImage - 1,
+      currentImage: this.state.currentImage - 1
     })
   }
-  gotoNext() {
+  gotoNext () {
     this.setState({
-      currentImage: this.state.currentImage + 1,
+      currentImage: this.state.currentImage + 1
     })
   }
-  handleClickImage() {
+  handleClickImage () {
     if (this.state.currentImage === this.props.images.length - 1) return
 
     this.gotoNext()
   }
 
-  render() {
+  render () {
     const siteTitle = 'Gatsby Starter - Strata'
     const siteDescription = 'Site description'
 
@@ -117,28 +89,23 @@ class HomeIndex extends React.Component {
       <Layout>
         <Helmet>
           <title>{siteTitle}</title>
-          <meta name="description" content={siteDescription} />
+          <meta name='description' content={siteDescription} />
         </Helmet>
 
-        <div id="main">
-          <section id="one">
-            <header className="major">
+        <div id='main'>
+          <section id='one'>
+            <header className='major'>
               <h2>A.D.Faris Portfolio</h2>
             </header>
             <p>
-              Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc
-              nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae
-              lobortis tortor primis integer massa adipiscing id nisi accumsan
-              pellentesque commodo blandit enim arcu non at amet id arcu magna.
-              Accumsan orci faucibus id eu lorem semper nunc nisi lorem
-              vulputate lorem neque cubilia.
+            I'm a New Jersey based Full-Stack Web Developer, specializing in back end development.
             </p>
-            <ul className="actions">
+            <ul className='actions'>
               {/* <li><a href='#' className='button'>Learn More</a></li> */}
             </ul>
           </section>
 
-          <section id="two">
+          <section id='two'>
             <h2>Recent Work</h2>
 
             <Gallery
@@ -147,90 +114,18 @@ class HomeIndex extends React.Component {
                   src,
                   thumbnail,
                   caption,
-                  description,
+                  description
                 })
               )}
             />
 
-            <ul className="actions">
+            <ul className='actions'>
               <li>
-                <a href="#" className="button">
+                {/* <a href='#' className='button'>
                   Full Portfolio
-                </a>
+                </a> */}
               </li>
             </ul>
-          </section>
-
-          <section id="three">
-            <h2>Get In Touch</h2>
-            <p>
-              Accumsan pellentesque commodo blandit enim arcu non at amet id
-              arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi
-              lorem vulputate lorem neque lorem ipsum dolor.
-            </p>
-            <div className="row">
-              <div className="8u 12u$(small)">
-                <form method="post" action="#">
-                  <div className="row uniform 50%">
-                    <div className="6u 12u$(xsmall)">
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        placeholder="Name"
-                      />
-                    </div>
-                    <div className="6u 12u$(xsmall)">
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="Email"
-                      />
-                    </div>
-                    <div className="12u">
-                      <textarea
-                        name="message"
-                        id="message"
-                        placeholder="Message"
-                        rows="4"
-                      />
-                    </div>
-                  </div>
-                </form>
-                <ul className="actions">
-                  <li>
-                    <input type="submit" value="Send Message" />
-                  </li>
-                </ul>
-              </div>
-              <div className="4u 12u$(small)">
-                <ul className="labeled-icons">
-                  <li>
-                    <h3 className="icon fa-home">
-                      <span className="label">Address</span>
-                    </h3>
-                    1234 Somewhere Rd.
-                    <br />
-                    Nashville, TN 00000
-                    <br />
-                    United States
-                  </li>
-                  <li>
-                    <h3 className="icon fa-mobile">
-                      <span className="label">Phone</span>
-                    </h3>
-                    000-000-0000
-                  </li>
-                  <li>
-                    <h3 className="icon fa-envelope-o">
-                      <span className="label">Email</span>
-                    </h3>
-                    <a href="#">hello@untitled.tld</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
           </section>
         </div>
       </Layout>
