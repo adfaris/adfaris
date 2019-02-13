fb45fa965a93d59a4c89c4578999d31ad70736ce
A.D.Faris <adfaris@gmail.com>
2019-02-12 23:21:44 -0500

    adding pics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,22 @@
-module.exports = {
-  siteMetadata: {
-    title: "Gatsby Starter - Strata by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Strata by HTML5 UP"
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
-      },
+export const siteMetadata = {
+  title: "Gatsby Starter - Strata by HTML5 UP",
+  author: "Hunter Chang",
+  description: "A Gatsby.js Starter based on Strata by HTML5 UP"
+};
+export const plugins = [
+  'gatsby-plugin-react-helmet',
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: 'gatsby-starter-default',
+      short_name: 'starter',
+      start_url: '/',
+      background_color: '#000',
+      theme_color: '#777',
+      display: 'minimal-ui',
+      icon: 'src/assets/images/website-icon.png',
     },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
-  ],
-}
+  },
+  'gatsby-plugin-sass',
+  'gatsby-plugin-offline'
+];
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
+import index from  '../pages/index'
 
 class Header extends React.Component {
   render () {
     return (
       <header id='header'>
         <div className='inner'>
-          <a href='#' className='image avatar'>
+          {/* <a href='index' className='image avatar'>
             <img src={avatar} alt='' />
-          </a>
+          </a> */}
           <h1>A.D.Faris portfolio page</h1>
+          <span>email: adfaris@gmail.com</span>
         </div>
         <Footer />
       </header>
